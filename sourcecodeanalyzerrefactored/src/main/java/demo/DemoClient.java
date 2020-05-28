78fcdc05177691b991226c688d4dbf43f4d6f501
Antonis Gkortzis <antonis.gkortzis@gmail.com>
2020-05-28 17:58:04 +0300

    Add bonus content
    - Add manually created expexted metrics file
    - Modify travis to execute the refactored analyzer
    - Compare the expected metrics with the output of the execution

--- sourcecodeanalyzerrefactored/src/main/java/demo/DemoClient.java
+++ sourcecodeanalyzerrefactored/src/main/java/demo/DemoClient.java
@@ -9,7 +9,7 @@ public class DemoClient {
 	public static void main(String[] args) throws IOException {
 		if(args.length != 5) {
 			System.err.println("Invalid number of arguments");
-			System.out.println("Execute as java –jar “jar-with-dependencies” arg0 arg1 arg2 arg3 arg4");
+			System.out.println("Execute as java -jar jar-with-dependencies arg0 arg1 arg2 arg3 arg4");
 			System.out.println("arg0 = JavaSourceCodeInputFile (e.g., src/test/resources/TestClass.java)\r\n" + 
 					"arg1 = sourceCodeAnalyzerType [regex|strcomp]\r\n" + 
 					"arg2 = SourceCodeLocationType [local|web]\r\n" + 
